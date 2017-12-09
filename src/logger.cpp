503aa82bac044f659a08a6c028e15cea8dd57213
Michel Promonet <michel.promonet@free.fr>
2017-12-09 17:15:04 +0100

    fix build without log4cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,5 +1,14 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** logger.cpp
+** 
+** -------------------------------------------------------------------------*/
+
 #include "logger.h"
 
-#ifdef HAVE_LOG4CPP
+#ifndef HAVE_LOG4CPP
 int LogLevel=NOTICE;
 #endif
